bf651e14f4614349646dcca3eac55e31e0d17a23
Michael Treger <michael.treger@gmail.com>
2012-12-24 10:45:33 -0800

    fixed spaces in name and reversed loop order

--- scraper.rb
+++ scraper.rb
@@ -2,18 +2,17 @@ require 'wombat'
 require 'pp'
 require 'sqlite3'
 
-db = SQLite3::Database.new "fantasy.db"
+db = SQLite3::Database.new "fantasy2.db"
 db.execute "CREATE TABLE IF NOT EXISTS fantasy (player_name TEXT, fteam INT, fgm INT, fga INT, ftm INT, fta INT, reb INT, ast INT, stl INT, blk INT, tover INT, pts INT, fpts INT, opp TEXT, slot TEXT, period_id INT)"
 
 opening_night = Time.parse("30/10/2012")
 one_day = 60*60*24
 current_period = ((Time.now - opening_night) / one_day).to_i
 
-#last_update = (db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0]+1
-last_update = 1
+last_update = ((db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0] or 0) + 1
 
-(1..0).each do |team_id|
-  (last_update..current_period).each do |period_id|
+(last_update..current_period).each do |period_id|
+  (1..8).each do |team_id|
     data = Wombat.crawl do  
       base_url "http://games.espn.go.com"
       path "/fba/clubhouse?leagueId=202659&teamId=#{team_id}&seasonId=2013&scoringPeriodId=#{period_id}"
@@ -30,7 +29,7 @@ last_update = 1
       if stats[0] == '--'
         next
       end
-      p["name"].gsub!(/[`'"]/,"")
+      p["name"].gsub!(/[`'"]|(\ \ )/," ")
       query = "INSERT INTO fantasy VALUES ('#{p["name"]}', #{team_id}, #{stats[0]}, #{stats[1]}, #{stats[2]}, #{stats[3]}, #{stats[4]}, #{stats[5]}, #{stats[6]}, #{stats[7]}, #{stats[8]}, #{stats[9]}, #{stats[10]}, '#{p["opp"]}', '#{p["slot"]}', #{period_id});"
       pp query
       db.execute query
@@ -41,8 +40,8 @@ end
 ###########################
 # Insert free agent records
 ###########################
-(0..250).step(50).each do |start_idx|
-  (last_update..current_period).each do |period_id|
+(last_update..current_period).each do |period_id|
+  (0..250).step(50).each do |start_idx|
     data = Wombat.crawl do  
       base_url "http://games.espn.go.com"
       path "/fba/leaders?leagueId=202659&scoringPeriodId=#{period_id}&seasonId=2013&startIndex=#{start_idx}"
@@ -58,7 +57,7 @@ end
       if stats[0] == '--'
         break
       end
-      p["name"].gsub!(/[`']/,"")
+      p["name"].gsub!(/[`'"]|(\ \ )/," ")
       if db.execute("select count(*) from fantasy where player_name = '#{p["name"]}' and period_id = #{period_id};")[0][0] == 0
         query = "INSERT INTO fantasy VALUES ('#{p["name"]}', '', #{stats[0]}, #{stats[1]}, #{stats[2]}, #{stats[3]}, #{stats[4]}, #{stats[5]}, #{stats[6]}, #{stats[7]}, #{stats[8]}, #{stats[9]}, #{stats[10]}, '#{p["opp"]}', 'FA', #{period_id});"
         pp query
