850f3f14cd249628572c9853ef99e6f4cb3f3aa7
Michael Treger <michael.treger@gmail.com>
2012-12-23 23:14:49 -0800

    Added free agents

--- scraper.rb
+++ scraper.rb
@@ -9,9 +9,10 @@ opening_night = Time.parse("30/10/2012")
 one_day = 60*60*24
 current_period = ((Time.now - opening_night) / one_day).to_i
 
-last_update = (db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0]+1
+#last_update = (db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0]+1
+last_update = 1
 
-(1..8).each do |team_id|
+(1..0).each do |team_id|
   (last_update..current_period).each do |period_id|
     data = Wombat.crawl do  
       base_url "http://games.espn.go.com"
@@ -29,6 +30,7 @@ last_update = (db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0]+1
       if stats[0] == '--'
         next
       end
+      p["name"].gsub!(/[`'"]/,"")
       query = "INSERT INTO fantasy VALUES ('#{p["name"]}', #{team_id}, #{stats[0]}, #{stats[1]}, #{stats[2]}, #{stats[3]}, #{stats[4]}, #{stats[5]}, #{stats[6]}, #{stats[7]}, #{stats[8]}, #{stats[9]}, #{stats[10]}, '#{p["opp"]}', '#{p["slot"]}', #{period_id});"
       pp query
       db.execute query
@@ -36,4 +38,34 @@ last_update = (db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0]+1
   end      
 end
 
-db.close
+###########################
+# Insert free agent records
+###########################
+(0..250).step(50).each do |start_idx|
+  (last_update..current_period).each do |period_id|
+    data = Wombat.crawl do  
+      base_url "http://games.espn.go.com"
+      path "/fba/leaders?leagueId=202659&scoringPeriodId=#{period_id}&seasonId=2013&startIndex=#{start_idx}"
+
+      players "css=.pncPlayerRow", :iterator do
+        name  "css=td.playertablePlayerName a"
+        opp   "css=td div a"
+        stats "css=td.playertableStat", :list
+      end
+    end
+    data["players"].each do |p|
+      stats = p["stats"]
+      if stats[0] == '--'
+        break
+      end
+      p["name"].gsub!(/[`']/,"")
+      if db.execute("select count(*) from fantasy where player_name = '#{p["name"]}' and period_id = #{period_id};")[0][0] == 0
+        query = "INSERT INTO fantasy VALUES ('#{p["name"]}', '', #{stats[0]}, #{stats[1]}, #{stats[2]}, #{stats[3]}, #{stats[4]}, #{stats[5]}, #{stats[6]}, #{stats[7]}, #{stats[8]}, #{stats[9]}, #{stats[10]}, '#{p["opp"]}', 'FA', #{period_id});"
+        pp query
+        db.execute query
+      end
+    end
+  end      
+end
+
+db.close  
