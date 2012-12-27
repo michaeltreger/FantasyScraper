9b87bb442347f9aed86472c25e7fbbceed0a5503
ahirreddy <ahirreddy@gmail.com>
2012-12-26 16:48:21 -0800

    working with postgres

--- scraper.rb
+++ scraper.rb
@@ -1,15 +1,22 @@
 require 'wombat'
 require 'pp'
-require 'sqlite3'
+require 'pg'
 
-db = SQLite3::Database.new "fantasy.db"
-db.execute "CREATE TABLE IF NOT EXISTS fantasy (player_name TEXT, team TEXT, fteam INT, min INT, fgm INT, fga INT, ftm INT, fta INT, reb INT, ast INT, stl INT, blk INT, tover INT, pts INT, fpts INT, opp TEXT, slot TEXT, period_id INT) "
+db = PGconn.open(:host => 'localhost', :port => 5432)
+
+# Create Database Fantasy if it does not exist
+res = db.exec "SELECT 1 FROM pg_database WHERE datname='fantasy'"
+if res.ntuples == 0
+  db.exec "CREATE DATABASE fantasy"
+end
+
+db.exec "CREATE TABLE IF NOT EXISTS fantasy (player_name TEXT, team TEXT, fteam INT, min INT, fgm INT, fga INT, ftm INT, fta INT, reb INT, ast INT, stl INT, blk INT, tover INT, pts INT, fpts INT, opp TEXT, slot TEXT, period_id INT)"
 
 opening_night = Time.parse("30/10/2012")
 one_day = 60*60*24
 current_period = ((Time.now - opening_night) / one_day).to_i
 
-last_update = ((db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0] or 0) + 1
+last_update = ((db.exec "SELECT MAX(period_id) FROM fantasy;")[0][0] or 0) + 1
 
 (last_update..current_period).each do |period_id|
   ###########################
@@ -35,7 +42,7 @@ last_update = ((db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0] or 0) + 
       team = fullName[1][0,3]
       query = "INSERT INTO fantasy VALUES ('#{name}', '#{team}',  #{fteam}, '', #{stats[0]}, #{stats[1]}, #{stats[2]}, #{stats[3]}, #{stats[4]}, #{stats[5]}, #{stats[6]}, #{stats[7]}, #{stats[8]}, #{stats[9]}, #{stats[10]}, '#{p["opp"]}', '#{p["slot"]}', #{period_id});"
       pp query
-      db.execute query
+      db.exec query
     end
   end
 
@@ -59,14 +66,14 @@ last_update = ((db.execute "SELECT MAX(period_id) FROM fantasy;")[0][0] or 0) + 
       fullName = p["name"].split(/,\s*/)
       name = fullName[0].gsub(/[`'"]|(\s+)/," ").gsub(/\*/, "")
       team = fullName[1][0,3]
-      if db.execute("SELECT count(*) FROM fantasy WHERE player_name = '#{name}' AND period_id = #{period_id};")[0][0] == 0
+      if db.exec("SELECT count(*) FROM fantasy WHERE player_name = '#{name}' AND period_id = #{period_id};")[0][0] == 0
         query = "INSERT INTO fantasy VALUES ('#{name}', '#{team}', '', #{stats[0]}, #{stats[1]}, #{stats[2]}, #{stats[3]}, #{stats[4]}, #{stats[5]}, #{stats[6]}, #{stats[7]}, #{stats[8]}, #{stats[9]}, #{stats[10]}, #{stats[11]}, '#{p["opp"]}', 'FA', #{period_id});"
         pp query
-        db.execute query
+        db.exec query
       else
-	query = "UPDATE fantasy SET min = #{stats[0]} WHERE player_name = '#{name}' AND period_id = #{period_id};"
+	      query = "UPDATE fantasy SET min = #{stats[0]} WHERE player_name = '#{name}' AND period_id = #{period_id};"
         pp query
-        db.execute query
+        db.exec query
       end
     end
   end
