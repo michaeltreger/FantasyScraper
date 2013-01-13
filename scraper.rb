41806b4db53ad6bab5ac47dbbb30232b02bbdc05
ahirreddy <ahirreddy@gmail.com>
2013-01-13 02:19:33 -0800

    added primary key constraint

--- scraper.rb
+++ scraper.rb
@@ -41,7 +41,7 @@ end
 ############################
 # Insert Team Player Records
 ############################
-db.exec "CREATE TABLE IF NOT EXISTS fantasy (player_name TEXT, team TEXT, fteam INT, min INT, fgm INT, fga INT, ftm INT, fta INT, reb INT, ast INT, stl INT, blk INT, tover INT, pts INT, fpts INT, opp TEXT, slot TEXT, period_id INT)"
+db.exec "CREATE TABLE IF NOT EXISTS fantasy (player_name TEXT, team TEXT, fteam INT, min INT, fgm INT, fga INT, ftm INT, fta INT, reb INT, ast INT, stl INT, blk INT, tover INT, pts INT, fpts INT, opp TEXT, slot TEXT, period_id INT, PRIMARY KEY(player_name, period_id))"
 
 opening_night = Time.parse("30/10/2012")
 one_day = 60*60*24
